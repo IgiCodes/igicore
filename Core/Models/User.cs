abf7ef2a4a0bc8960aa3f84e27cee52f4770d083
Joe Biellik <joebiellik@gmail.com>
2018-03-09 02:29:17 +0000

    Remove unused references

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System;
 
 namespace IgiCore.Core.Models
