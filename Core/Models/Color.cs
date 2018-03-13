2c44c20908138cc9905f87e0094f9c2927e8d554
Joe Biellik <joebiellik@gmail.com>
2018-03-13 15:02:33 +0000

    Add Color type

---
+++ Core/Models/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace IgiCore.Core.Models
+{
+    public struct Color
+    {
+        public short Red { get; set; }
+        public short Green { get; set; }
+        public short Blue { get; set; }
+    }
+}
