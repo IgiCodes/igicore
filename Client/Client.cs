cc87d8cb784df335b24592661927077b1a039b1a
Igi <igirisujinsenpai@gmail.com>
2018-03-18 21:48:09 +0000

    Woops, missed a bit

--- Client/Client.cs
+++ Client/Client.cs
@@ -64,11 +64,7 @@ namespace IgiCore.Client
 
             TriggerServerEvent("igi:vehicle:save", JsonConvert.SerializeObject(car));
 
-<<<<<<< HEAD
-            //this.Services.First<VehicleService>().Tracked.Add(car.Handle);
-=======
             this.Services.First<VehicleService>().Tracked.Add(car.Handle);
->>>>>>> 964748f2d95f9f22e29e87adb203ae3502a5a3c8
         }
 
         protected async void UserLoad(User user)
