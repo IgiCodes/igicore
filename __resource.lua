58fea9f292a7352b549316d6d5900c0eff843c03
Joe Biellik <joebiellik@gmail.com>
2018-04-25 16:44:25 +0100

    Re-add loading screen

--- __resource.lua
+++ __resource.lua
@@ -18,10 +18,23 @@ files {
 	'EntityFramework.dll',
 	'NativeUI.dll',
 
+	'Interface/loading-screen/index.html',
+	'Interface/loading-screen/main.js',
+	'Interface/loading-screen/main.css',
+	'Interface/loading-screen/libs/jquery.min.js',
+	'Interface/loading-screen/libs/bootstrap.min.css',
+	'Interface/loading-screen/img/logo.png',
+	'Interface/loading-screen/img/bg1.jpg',
+	'Interface/loading-screen/img/bg2.jpg',
+	'Interface/loading-screen/img/bg3.jpg',
+	'Interface/loading-screen/img/bg4.jpg',
+	'Interface/loading-screen/img/bg5.jpg',
+
 	'Interface/dist/index.html',
 	'Interface/dist/bundle.js',
 	'Interface/dist/gravity.woff2',
 	'Interface/dist/pricedown.woff2',
 }
 
+loadscreen 'Interface/loading-screen/index.html'
 ui_page 'Interface/dist/index.html'
