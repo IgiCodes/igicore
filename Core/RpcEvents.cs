6ffbb7342b9d007f76951ab4b149ed8df8502ab3
Joe Biellik <joebiellik@gmail.com>
2018-05-01 17:52:35 +0100

    Define events statically

---
+++ Core/RpcEvents.cs
@@ -0,0 +1,13 @@
+﻿namespace IgiCore.Core
+{
+	public static class RpcEvents
+	{
+		public const string GetServerInformation = "igi:client:ready";
+
+		public const string GetUser = "igi:user:load";
+		public const string GetCharacters = "igi:user:characters";
+
+
+		public const string CharacterLoad = "igi:character:load";
+	}
+}
