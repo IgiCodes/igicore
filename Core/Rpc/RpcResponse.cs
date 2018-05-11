9b4858f560c36d29d9ae7973433710b78b08909a
Joe Biellik <joebiellik@gmail.com>
2018-05-11 18:55:17 +0100

    Simplify RpcResponse abstraction

--- Core/Rpc/RpcResponse.cs
+++ Core/Rpc/RpcResponse.cs
@@ -5,7 +5,7 @@ namespace IgiCore.Core.Rpc
 {
 	public class RpcResponse<T> : RpcMessage
 	{
-		private readonly Lazy<T> result;
+		protected readonly Lazy<T> result;
 
 		public T Result => this.result.Value;
 
@@ -22,8 +22,8 @@ namespace IgiCore.Core.Rpc
 
 	public class RpcResponse<T1, T2> : RpcMessage
 	{
-		private readonly Lazy<T1> result1;
-		private readonly Lazy<T2> result2;
+		protected readonly Lazy<T1> result1;
+		protected readonly Lazy<T2> result2;
 
 		public T1 Result1 => this.result1.Value;
 		public T2 Result2 => this.result2.Value;
@@ -40,79 +40,52 @@ namespace IgiCore.Core.Rpc
 		}
 	}
 
-	public class RpcResponse<T1, T2, T3> : RpcMessage
+	public class RpcResponse<T1, T2, T3> : RpcResponse<T1, T2>
 	{
-		private readonly Lazy<T1> result1;
-		private readonly Lazy<T2> result2;
-		private readonly Lazy<T3> result3;
+		protected readonly Lazy<T3> result3;
 
-		public T1 Result1 => this.result1.Value;
-		public T2 Result2 => this.result2.Value;
 		public T3 Result3 => this.result3.Value;
 
 		public RpcResponse()
 		{
-			this.result1 = new Lazy<T1>(() => JsonConvert.DeserializeObject<T1>(this.Payloads[0]));
-			this.result2 = new Lazy<T2>(() => JsonConvert.DeserializeObject<T2>(this.Payloads[1]));
 			this.result3 = new Lazy<T3>(() => JsonConvert.DeserializeObject<T3>(this.Payloads[2]));
 		}
 
-		public static RpcResponse<T1, T2, T3> Parse(string result)
+		public new static RpcResponse<T1, T2, T3> Parse(string result)
 		{
 			return JsonConvert.DeserializeObject<RpcResponse<T1, T2, T3>>(result);
 		}
 	}
 
-	public class RpcResponse<T1, T2, T3, T4> : RpcMessage
+	public class RpcResponse<T1, T2, T3, T4> : RpcResponse<T1, T2, T3>
 	{
-		private readonly Lazy<T1> result1;
-		private readonly Lazy<T2> result2;
-		private readonly Lazy<T3> result3;
-		private readonly Lazy<T4> result4;
+		protected readonly Lazy<T4> result4;
 
-		public T1 Result1 => this.result1.Value;
-		public T2 Result2 => this.result2.Value;
-		public T3 Result3 => this.result3.Value;
 		public T4 Result4 => this.result4.Value;
 
 		public RpcResponse()
 		{
-			this.result1 = new Lazy<T1>(() => JsonConvert.DeserializeObject<T1>(this.Payloads[0]));
-			this.result2 = new Lazy<T2>(() => JsonConvert.DeserializeObject<T2>(this.Payloads[1]));
-			this.result3 = new Lazy<T3>(() => JsonConvert.DeserializeObject<T3>(this.Payloads[2]));
 			this.result4 = new Lazy<T4>(() => JsonConvert.DeserializeObject<T4>(this.Payloads[3]));
 		}
 
-		public static RpcResponse<T1, T2, T3, T4> Parse(string result)
+		public new static RpcResponse<T1, T2, T3, T4> Parse(string result)
 		{
 			return JsonConvert.DeserializeObject<RpcResponse<T1, T2, T3, T4>>(result);
 		}
 	}
 
-	public class RpcResponse<T1, T2, T3, T4, T5> : RpcMessage
+	public class RpcResponse<T1, T2, T3, T4, T5> : RpcResponse<T1, T2, T3, T4>
 	{
-		private readonly Lazy<T1> result1;
-		private readonly Lazy<T2> result2;
-		private readonly Lazy<T3> result3;
-		private readonly Lazy<T4> result4;
-		private readonly Lazy<T5> result5;
+		protected readonly Lazy<T5> result5;
 
-		public T1 Result1 => this.result1.Value;
-		public T2 Result2 => this.result2.Value;
-		public T3 Result3 => this.result3.Value;
-		public T4 Result4 => this.result4.Value;
 		public T5 Result5 => this.result5.Value;
 
 		public RpcResponse()
 		{
-			this.result1 = new Lazy<T1>(() => JsonConvert.DeserializeObject<T1>(this.Payloads[0]));
-			this.result2 = new Lazy<T2>(() => JsonConvert.DeserializeObject<T2>(this.Payloads[1]));
-			this.result3 = new Lazy<T3>(() => JsonConvert.DeserializeObject<T3>(this.Payloads[2]));
-			this.result4 = new Lazy<T4>(() => JsonConvert.DeserializeObject<T4>(this.Payloads[3]));
 			this.result5 = new Lazy<T5>(() => JsonConvert.DeserializeObject<T5>(this.Payloads[4]));
 		}
 
-		public static RpcResponse<T1, T2, T3, T4, T5> Parse(string result)
+		public new static RpcResponse<T1, T2, T3, T4, T5> Parse(string result)
 		{
 			return JsonConvert.DeserializeObject<RpcResponse<T1, T2, T3, T4, T5>>(result);
 		}
