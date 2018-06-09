4c1d85e56ee214e88458a9c32d8930aa99bd217c
Igi <igirisujinsenpai@gmail.com>
2018-06-09 01:38:26 +0000

    Initial structure for Roleplay Plugin

---
+++ Plugins/roleplay/Server/Configuration.cs
@@ -0,0 +1,9 @@
+﻿using IgiCore.SDK.Core.Controllers;
+
+namespace Roleplay.Server
+{
+	public class Configuration : IControllerConfiguration
+	{
+		public string Test { get; set; }
+	}
+}
---
+++ Plugins/roleplay/Server/RoleplayController.cs
@@ -0,0 +1,17 @@
+﻿using IgiCore.SDK.Core.Diagnostics;
+using IgiCore.SDK.Server.Controllers;
+using IgiCore.SDK.Server.Events;
+using IgiCore.SDK.Server.Rpc;
+
+
+namespace Roleplay.Server
+{
+	public class RoleplayController : ConfigurableController<Configuration>
+	{
+		public RoleplayController(ILogger logger, IEventManager events, IRpcHandler rpc, Configuration configuration) : base(logger, events, rpc, configuration)
+		{
+			this.Logger.Debug(this.Configuration.Test);
+		}
+
+	}
+}
