72854da16424ec9440b5cb1f0f0878024e204d05
Joe Biellik <joebiellik@gmail.com>
2018-06-09 17:56:59 +0100

    Add example client service

---
+++ Plugins/roleplay/Client/ExampleService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using IgiCore.SDK.Client.Events;
+using IgiCore.SDK.Client.Rpc;
+using IgiCore.SDK.Client.Services;
+using IgiCore.SDK.Core.Diagnostics;
+
+namespace Roleplay.Client
+{
+	public class ExampleService : Service
+	{
+		public ExampleService(ILogger logger, ITickManager ticks, IEventManager events, IRpcHandler rpc) : base(logger, ticks, events, rpc)
+		{
+			this.Ticks.Attach(Tick);
+		}
+
+		private async Task Tick()
+		{
+			await Task.FromResult(0);
+		}
+	}
+}
