e48db23980c5458c499df302eef5dbe04839b67f
Igi <igirisujinsenpai@gmail.com>
2018-06-09 00:53:37 +0000

    Added multi-param support to Server Events

--- Server/Events/EventManager.cs
+++ Server/Events/EventManager.cs
@@ -9,7 +9,7 @@ namespace IgiCore.Server.Events
 	{
 		private readonly Dictionary<string, List<Subscription>> subscriptions = new Dictionary<string, List<Subscription>>();
 
-		public void On<T>(string @event, Action<T> action)
+		private void InternalOn(string @event, Delegate action)
 		{
 			lock (this.subscriptions)
 			{
@@ -22,7 +22,20 @@ namespace IgiCore.Server.Events
 			}
 		}
 
-		public void Raise<T>(string @event, T message)
+		public void On(string @event, Action action) => InternalOn(@event, action);
+
+		public void On<T>(string @event, Action<T> action) => InternalOn(@event, action);
+
+		public void On<T1, T2>(string @event, Action<T1, T2> action) => InternalOn(@event, action);
+
+		public void On<T1, T2, T3>(string @event, Action<T1, T2, T3> action) => InternalOn(@event, action);
+
+		public void On<T1, T2, T3, T4>(string @event, Action<T1, T2, T3, T4> action) => InternalOn(@event, action);
+
+		public void On<T1, T2, T3, T4, T5>(string @event, Action<T1, T2, T3, T4, T5> action) => InternalOn(@event, action);
+
+
+		private void InternalRaise(string @event, params object[] args)
 		{
 			lock (this.subscriptions)
 			{
@@ -30,33 +43,59 @@ namespace IgiCore.Server.Events
 
 				foreach (var subscription in this.subscriptions[@event])
 				{
-					subscription.Handle(message);
+					subscription.Handle(args);
 				}
 			}
 		}
 
-		public async Task RaiseAsync<T>(string @event, T message)
+		public void Raise(string @event) => InternalRaise(@event);
+
+		public void Raise<T>(string @event, T p1) => InternalRaise(@event, p1);
+
+		public void Raise<T1, T2>(string @event, T1 p1, T2 p2) => InternalRaise(@event, p1, p2);
+
+		public void Raise<T1, T2, T3>(string @event, T1 p1, T2 p2, T3 p3) => InternalRaise(@event, p1, p2, p3);
+
+		public void Raise<T1, T2, T3, T4>(string @event, T1 p1, T2 p2, T3 p3, T4 p4) => InternalRaise(@event, p1, p2, p3, p4);
+
+		public void Raise<T1, T2, T3, T4, T5>(string @event, T1 p1, T2 p2, T3 p3, T4 p4, T5 p5) => InternalRaise(@event, p1, p2, p3, p4, p5);
+
+
+		private async Task InternalRaiseAsync(string @event, params object[] args)
 		{
 			await Task.Factory.StartNew(() =>
 			{
-				Raise(@event, message);
+				InternalRaise(@event, args);
 			});
 		}
 
+		public Task RaiseAsync(string @event) => InternalRaiseAsync(@event);
+
+		public Task RaiseAsync<T>(string @event, T p1) => InternalRaiseAsync(@event, p1);
+
+		public Task RaiseAsync<T1, T2>(string @event, T1 p1, T2 p2) => InternalRaiseAsync(@event, p1, p2);
+
+		public Task RaiseAsync<T1, T2, T3>(string @event, T1 p1, T2 p2, T3 p3) => InternalRaiseAsync(@event, p1, p2, p3);
+
+		public Task RaiseAsync<T1, T2, T3, T4>(string @event, T1 p1, T2 p2, T3 p3, T4 p4) => InternalRaiseAsync(@event, p1, p2, p3, p4);
+
+		public Task RaiseAsync<T1, T2, T3, T4, T5>(string @event, T1 p1, T2 p2, T3 p3, T4 p4, T5 p5) => InternalRaiseAsync(@event, p1, p2, p3, p4, p5);
+
+
 		public class Subscription
 		{
-			private readonly object handler;
+			private readonly Delegate handler;
 
-			public Subscription(object handler)
+			public Subscription(Delegate handler)
 			{
 				this.handler = handler;
 			}
 
-			public bool Handle<T>(T message)
+			public bool Handle(params object[] args)
 			{
 				bool cancel = false;
 
-				(this.handler as Action<T>)(message);
+				this.handler.DynamicInvoke(args);
 
 				return cancel;
 			}
